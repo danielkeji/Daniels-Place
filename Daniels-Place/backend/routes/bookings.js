f717cbf98d7f30ca3870b3540281d9039573ae04
KEJI Daniel Aliyu <danielkeji123@gmail.com>
2025-07-08 09:43:01 -0700

    Seperated backend and front end

--- Daniels-Place/backend/routes/bookings.js
+++ Daniels-Place/backend/routes/bookings.js
@@ -2,13 +2,10 @@ import { Router } from "express";
 import {
   createBooking,
   generatePaymentLink,
-  confirmBooking,
 } from "../controllers/bookingControllers.js";
 const router = Router();
 
-
 router.post("/", createBooking);
 router.post("/pay", generatePaymentLink);
-router.post("/confirm", confirmBooking);
 
 export default router;
